43fa03a213280682dc10849564f44c5e032813e8
Juan Marin Otero <juan.marin.otero@gmail.com>
2014-12-04 16:38:39 -0500

    Partial interpolator messages

--- src/main/scala/actors/LineInterpolator.scala
+++ src/main/scala/actors/LineInterpolator.scala
@@ -5,10 +5,17 @@ import geometry._
 
 object LineInterpolator {
   case class Interpolate(line: Line, d: Double)
+  sealed trait AddressRange
+  case class LeftAddressRange(start: Int, end: Int) extends AddressRange
+  case class RightAddressRange(start: Int, end: Int) extends AddressRange
 }
 
 class LineInterpolator extends Actor with ActorLogging {
   import LineInterpolator._
+
+  def splitAddressRange(l1: Int, l2: Int, d: Double): Double = {
+
+  }
 
   def receive: Receive = {
     case Interpolate(line, d) =>
