9a61e89f780abb5db71cef887bf6afd5aa951ce3
Juan Marin Otero <juan.marin.otero@gmail.com>
2014-12-17 14:46:26 -0500

    Add map

--- src/main/resources/web/js/suggestAddress.js
+++ src/main/resources/web/js/suggestAddress.js
@@ -1,4 +1,14 @@
  $(function() {
+
+   var map = L.map('map').setView([38.8951100, -77.0363700], 13);
+
+   L.tileLayer('http://{s}.tiles.mapbox.com/v3/examples.map-i87786ca/{z}/{x}/{y}.png', {
+       attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+       maxZoom: 18
+   }).addTo(map);
+
+   var layer = L.geoJson();
+
    function log(message) {
      console.log(message);
    }
@@ -22,8 +32,19 @@
      minLength: 3,
      delay: 300,
      select: function(event, ui) {
-       log(ui.item)
+       layer.clearLayers();
+       var feature = ui.item.feature;
+       //log(feature);
+       var coordinates = feature.geometry.coordinates;
+       layer.addData(feature);
+       layer.addTo(map);
+       map.setView([coordinates[1], coordinates[0]], 13);
      }
    });
+
+   function removeLayer(layer) {
+     map.removeLayer(layer);
+   }
+
  });
 
